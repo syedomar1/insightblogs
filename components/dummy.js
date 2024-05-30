d18a9e5f4ce8bafaa36006cb1eef65396885d829
Syed Omar Albeez <syedomar.albeez2022@vitstudent.ac.in>
2024-05-30 19:56:30 +0530

    Experimenting styling

---
+++ components/dummy.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Dummy = () => {
+  return (
+    <>
+    <style jsx global>
+        {`
+            div{
+                color: red;
+            }
+            `}
+    </style>
+    <div>
+      I am dummy
+    </div>
+    </>
+  )
+}
+
+export default Dummy
