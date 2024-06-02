9dd4b8990feb76a8f70d9b416701d8d252398010
Syed Omar Albeez <syedomar.albeez2022@vitstudent.ac.in>
2024-06-02 20:53:16 +0530

    Created Contact API

---
+++ pages/api/postcontact.js
@@ -0,0 +1,16 @@
+import * as fs from "fs";
+
+export default async function handler(req, res){
+    if(req.method === 'POST'){
+        console.log(req.body);
+        let data = await fs.promises.readdir('contactdata');
+        fs.promises.writeFile(`contactdata/${data.length+1}.json`,JSON.stringify(req.body))
+        res.status(200).json(req.body);
+    }
+    else{
+        //Handle any other HTTP method
+        res.status(200).json(["allBlogs"]);
+        // name, email, desc, phone
+         
+    }
+}
